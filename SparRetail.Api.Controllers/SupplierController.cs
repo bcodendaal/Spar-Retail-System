ad55d567b301a160d1bf945946966d3b04171ef7
pepsinmentos <pieter.roodt@gmail.com>
2015-02-02 21:12:58 +0200

    api check in

---
+++ SparRetail.Api.Controllers/SupplierController.cs
@@ -0,0 +1,25 @@
+﻿using SparRetail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace SparRetail.Api.Controllers
+{
+    public class SupplierController : ApiController
+    {
+        public string Test()
+        {
+            return "Test works";
+        }
+
+        [HttpGet]
+        public Supplier All()
+        {
+            return new Supplier();
+        }
+    }
+}
