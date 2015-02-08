68279d09d32c2fb521c348e1534c1fdfcd3c48f1
bcodendaal <bcodendaal@gmail.com>
2015-02-08 18:52:00 +0200

    test

--- SparRetail.UI.Controllers/Class1.cs
+++ SparRetail.UI.Controllers/Class1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SparRetail.UI.Controllers
 {
-    public class Class1
+    public class Class1 : Controller
     {
     }
 }
