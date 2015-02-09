7c5e17ef2cb34fd4c198c55a46ef62d1c253fd6e
bcodendaal <bcodendaal@gmail.com>
2015-02-09 20:26:40 +0200

    test

---
+++ SparRetail.UI.Controllers/Retailer/RetailerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SparRetail.UI.Controllers.Retailer
+{
+    public class RetailerController : Controller
+    {
+
+        private ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SparRetail.UI/App_Start/RouteConfig.cs
+++ SparRetail.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BeyondThemes.BeyondAdmin
             routes.MapRoute(
                name: "Default",
                url: "{controller}/{action}/{id}",
-               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               defaults: new { controller = "Retailer", action = "Dashboard", id = UrlParameter.Optional }
            );
         }
     }
